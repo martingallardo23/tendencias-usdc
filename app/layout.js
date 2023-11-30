b7323c4e64334b3d92681c3e5f31e530fcdbc2e4
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2023-11-30 12:03:01 -0300

    Fix language

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,20 @@ import './globals.css'
 import LeftPanel from './LeftPanel'
 
 export const metadata = {
-  title: 'USDC Exchange Rates',
-  description: 'USD Coin exchange rates in Argentina',
+  title: 'USDC Trends',
+  description: 'USDC exchange rates in Argentina',
+  google: 'notranslate',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="Content-Language" content="es"/>
+      </head>
       <body>
         <LeftPanel />
         <div className="flex flex-row justify-center items-center m-auto">
