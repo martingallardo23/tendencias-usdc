3ef260c2402e09742395d4abc4430ef1b5e4572e
Martín Gallardo <69550249+martingallardo23@users.noreply.github.com>
2023-12-02 17:51:42 -0300

    fix httpequiv error

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <meta http-equiv="Content-Language" content="es"/>
+        <meta httpEquiv="Content-Language" content="es"/>
       </head>
       <body>
         <LeftPanel />
